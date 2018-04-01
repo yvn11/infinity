44ba90e07703df51edb6f88b1a2946ce3033c8ac
Zex Li <top_zlynch@yahoo.com>
2018-04-01 12:58:53 +0800

    update solution

--- algo_cpp/largest_rect_histogram.cpp
+++ algo_cpp/largest_rect_histogram.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <limits.h>
 
 using namespace std;
 
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
+    int with_table(vector<int>& heights) {
       if (heights.empty())
         return 0;
       
@@ -19,7 +20,7 @@ public:
         ma = max(ma, mem[1][i]);
       }
 
-      for (int i = 0; i <= n; ++i) {
+      for (int i = 0; i < n; ++i) {
         for (int l = 2; i+l <= n; ++l) {
           mem[l][i] = min(mem[l-1][i], heights[i+l-1]);
 
@@ -29,6 +30,40 @@ public:
         }
       }
       return ma;
+    }
+
+    int get_area(stack<int>& mem, vector<int>& heights, int cur) {
+      if (mem.empty())
+        return 0;
+      int t = INT_MAX;
+      int c = 0;
+      while (!mem.empty()) {
+          t = min(t, mem.top());
+          mem.pop();
+          c++;
+      }
+      return heights[t] * (mem.empty() ? c : cur-mem.top()-1);
+    }
+
+    int with_stack(vector<int>& heights) {
+      stack<int> mem;
+      int n = heights.size();
+      int i = 0, ma = 0;
+
+      while (i < n) {
+        if (mem.empty() || heights[mem.top()] <= heights[i]) {
+          mem.push(i++);
+        } else {
+          ma = max(ma, this->get_area(mem, heights, i));
+        }
+      }
+      ma = max(ma, this->get_area(mem, heights, i));
+      return ma;
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+      //return this->with_table(heights);
+      return this->with_stack(heights);
     }
 };
 
