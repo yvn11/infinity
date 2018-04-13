dbe040c8d74ae512820cd3d0dce223a9876ad318
Zex Li <top_zlynch@yahoo.com>
2018-04-13 22:16:01 +0800

    update solution

---
+++ algo_cpp/reorder_list.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include "list_node.h"
+
+using namespace std;
+
+void print_list(ListNode* head);
+
+class Solution {
+public:
+    
+    void reorderList(ListNode* head) {
+        if (!head)
+          return;
+
+        ListNode* mid = head, *p = head->next;
+        
+        while (p && p->next) {
+            mid = mid->next;
+            p = p->next->next;
+        }
+
+        if (!mid->next)
+          return;
+
+        p = mid->next;
+        ListNode *t = p->next, *q = NULL, *s = NULL;
+        
+        while (t) {
+            q = t->next;
+            t->next = p;
+            p = t; t = q;
+        }
+        
+        mid->next->next = NULL;
+        q = head;
+        
+        while (p) {
+            t = q->next;
+            s = p->next;
+            q->next = p;
+            p->next = t;
+            q = t; p = s;
+        }
+        
+        if (q->next)
+          q->next = NULL;
+    }
+};
+
+void print_list(ListNode *head) {
+  ListNode *p = head;
+
+  while (p) {
+    cout << p->val << ',';
+    p = p->next;
+  }
+  cout << endl;
+}
+
+int main() {
+  Solution s;
+  ListNode *head = new ListNode(1), *p;
+
+  p = head;
+  for (int i = 2; i <= 9; ++i) {
+    p->next = new ListNode(i);
+    p = p->next;
+  }
+
+  print_list(head);
+  s.reorderList(head);
+  print_list(head);
+  
+  return 0;
+}
