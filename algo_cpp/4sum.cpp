9f6924df9be5db0d3b2abea7a64ec192320b1ef5
Zex Li <top_zlynch@yahoo.com>
2018-04-26 19:32:20 +0800

    update solution

--- algo_cpp/4sum.cpp
+++ algo_cpp/4sum.cpp
@@ -1,84 +1,59 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
 #include <set>
-#include <map>
 #include <algorithm>
 
 using namespace std;
 
-struct Mem {
-    int sum;
-    int l;
-    int r;
-
-    Mem(int l, int r, int sum)
-        :l(l), r(r), sum(sum)
-        {}
-};
-
-
-struct {
-    bool operator()(Mem a, Mem b) const {
-        return a.sum < b.sum;
-    }
-} compare_mem;
-
 class Solution {
 public:
 
-    bool uniq(Mem &a, Mem &b) {
-      return !(a.l == b.l || a.l == b.r ||
-                    a.r == b.l || a.r == b.r);
-    }
-
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> ret;
+        if (nums.size() < 4)
+          return vector<vector<int>>();
 
-        if (nums.size() < 4) {
-            return ret;
-        }
+        set<vector<int>> ret;
+        unordered_map<int, set<pair<int, int>>> mem;
+        int n = nums.size();
 
-        vector<Mem> mem;
+        for (int i = 0; i < n-1; ++i) {
+          for (int j = i+1; j < n; ++j) {
+            int sum = nums[i] + nums[j];
 
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = i+1; j < nums.size(); ++j) {
-                mem.push_back(Mem(i, j, nums[i]+nums[j]));
-            }
-        }
-
-        sort(mem.begin(), mem.end(), compare_mem);
-        set<vector<int>> met;
-        int l = 0, r = mem.size()-1;
-
-        while (l < mem.size() && r >= 0) {
-            int v = mem[l].sum + mem[r].sum;
-
-            if (v == target) {
-                if (this->uniq(mem[l], mem[r])) {
-                    vector<int> v = {nums[mem[l].l], nums[mem[l].r],
-                      nums[mem[r].l], nums[mem[r].r]};
-                    sort(v.begin(), v.end());
-
-                    if (met.find(v) == met.end()) {
-                        ret.push_back(v);
-                        met.insert(v);
-                    }
+            if (mem.find(target-sum) != mem.end()) {
+              for (auto comb : mem[target-sum]) {
+                if (comb.first != i && comb.first != j
+                  && comb.second != i && comb.second != j) {
+                  vector<int> v{nums[comb.first], nums[comb.second], nums[i], nums[j]};
+                  sort(v.begin(), v.end());
+                  ret.insert(v);
                 }
+              }
             }
-
-            if (v < target) {
-                l++;
-            } else {
-                r--;
+            else {
+              pair<int, int> p = {i, j};
+              if (mem[sum].find(p) == mem[sum].end()) 
+                mem[sum].insert(p);
             }
+          }
         }
-        return ret;
+
+        vector<vector<int>> ret2;
+        for (auto v : ret) {
+          ret2.push_back(v);
+        }
+        return ret2;
     }
 };
 
 int main() {
   Solution s;
-  vector<int> v = {-5,-4,-3,-2,-1,0,0,1,2,3,4,5};
+  vector<int> v = 
+    {-3, -1, 0, 2, 4, 5};
+    //{1, 0, -1, 0, -2, 2};
+    //{-5,-4,-3,-2,-1,0,0,1,2,3,4,5};
   int t = 0;
   vector<vector<int>> r = s.fourSum(v, t);
 
