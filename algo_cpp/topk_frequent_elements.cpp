43ed1b03561a3f4ce92e863274bfea4c6ffe8ee9
Zex Li <top_zlynch@yahoo.com>
2018-04-27 14:50:21 +0800

    update solution

--- algo_cpp/topk_frequent_elements.cpp
+++ algo_cpp/topk_frequent_elements.cpp
@@ -15,7 +15,7 @@ struct compare{
 
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
+    vector<int> heap_way(vector<int>& nums, int k) {
         vector<int> ret;
         if (!nums.size() || !k)
           return ret;
@@ -34,6 +34,44 @@ public:
         }
 
         return ret;
+    }
+
+    vector<int> hash_way(vector<int>& nums, int k) {
+      unordered_map<int, vector<int>> mem;
+      vector<int> cache;
+      vector<int> ret;
+
+      sort(nums.begin(), nums.end());
+      int i = 0, freq = 1;
+
+      while (i < nums.size()) {
+        while (i < nums.size()-1 && nums[i] == nums[i+1]) {
+          ++freq; ++i;
+        }
+
+        if (mem.find(freq) == mem.end()) {
+          cache.push_back(freq);
+        }
+
+        mem[freq].push_back(nums[i]);
+        freq = 1;
+        ++i;
+      }
+
+      sort(cache.begin(), cache.end());
+
+      for (int i = cache.size()-1; i >= 0 && ret.size() < k; --i) {
+        int j = 0;
+        while (ret.size() < k && j < mem[cache[i]].size()) {
+          ret.push_back(mem[cache[i]][j++]);
+        }
+      }
+  
+      return ret;
+    }
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+      return heap_way(nums, k);
     }
 };
 
