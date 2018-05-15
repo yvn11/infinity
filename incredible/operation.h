1db6de6d11e25318f34aa8eb842c77e2657c5526
Zex Li <top_zlynch@yahoo.com>
2018-05-16 00:03:02 +0800

    define op result code

--- incredible/operation.h
+++ incredible/operation.h
@@ -6,9 +6,10 @@
 #include <vector>
 #include <numeric>
 
+namespace Incredible {
+
 using namespace std;
 
-namespace Incredible {
 
 struct OpResult {
   int code;
@@ -19,13 +20,37 @@ struct OpResult {
   {}
 };
 
-/*
+ostream& operator<< (ostream &o, OpResult &res) {
+  return o << res.code << ": " << res.msg;
+}
+
+struct OutputUpdated : OpResult {
+  OutputUpdated() : OpResult(0, "Output updated") {}
+};
+
+struct DependencyFail : OpResult {
+  DependencyFail() : OpResult(4, "Dependency failed") {}
+};
+
+struct NoEnoughSpace : OpResult {
+   NoEnoughSpace() : OpResult(2, "No enough space to store outputs") {}
+};
+
+struct NoEnoughInputs : OpResult {
+  NoEnoughInputs() : OpResult(3, "No enough parameters") {}
+};
+
+struct DivByZero : OpResult {
+  DivByZero() : OpResult(4, "Divided by zero") {}
+};
+
+
+
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 template<typename INPUT, typename OUTPUT>
-class Operation {
+class [[deprecated]] Operation {
     std::string _name;
   public:
-    //typedef typename INPUT Operation::INPUT INPUT;
-    //typedef typename Operation::OUTPUT OUTPUT;
     Operation<INPUT, OUTPUT>(std::string name) : _name(name) {};
     Operation<INPUT, OUTPUT>() : _name("op") {};
     void name(std::string name) { _name = name; }
@@ -33,5 +58,6 @@ class Operation {
     virtual OpResult run() { return OpResult(0, "Done"); }
     virtual OpResult run(vector<INPUT> &inputs, vector<OUTPUT> &outputs) { return OpResult(0, "Done"); };
 };
-*/
+
+
 } // namespace Incredible 
--- incredible/unit.h
+++ incredible/unit.h
@@ -8,10 +8,10 @@
 #include <algorithm>
 #include "operation.h"
 
-using namespace std;
 
 namespace Incredible {
 
+using namespace std;
 
 template<typename INPUT, typename OUTPUT>
 class Unit {
@@ -105,7 +105,7 @@ public:
   virtual OpResult run_all() {
     if (std::any_of(_depends.begin(), _depends.end(), 
           [&](auto &unit) { return (*unit).run().code != 0; }))
-      return OpResult(4, "Dependency failed");
+      return DependencyFail();
     return run();
   }
 };
@@ -122,149 +122,5 @@ ostream& operator<< (ostream &o, Unit<INPUT, OUTPUT> &unit) {
 
     return o << "]";
   }
-/* Perform addition on inputs */
-template<typename INPUT, typename OUTPUT>
-class Add : public Unit<INPUT, OUTPUT> {
-  public:
-    typedef Add<INPUT, OUTPUT> TYPE;
-    typedef Unit<INPUT, OUTPUT> BASE;
-    Add() : BASE() { BASE::name("Add"); }
-    Add(size_t n_inputs, size_t n_outputs) : BASE(n_inputs, n_outputs) { BASE::name("Add"); }
-    Add(vector<shared_ptr<INPUT>> &inputs, vector<shared_ptr<OUTPUT>> &outputs) : BASE(inputs, outputs) { BASE::name("Add"); };
-    Add(vector<shared_ptr<OUTPUT>> &inputs, size_t n_outputs) : BASE(inputs, n_outputs) { BASE::name("Add"); };
-
-    OpResult run() override {
-      if (BASE::inputs().size() < 2)
-        return OpResult(1, "No enough parameters");
-
-      if (BASE::outputs().size() < 1)
-        return OpResult(2, "No enough space to store outputs");
-
-      *BASE::outputs()[0] = *BASE::inputs()[0] + (*BASE::inputs()[1]);
-      return OpResult(0, "Output updated");
-    }
-};
-
-/* Perform substraction on inputs */
-template<typename INPUT, typename OUTPUT>
-class Sub : public Unit<INPUT, OUTPUT> {
-  public:
-    typedef Sub<INPUT, OUTPUT> TYPE;
-    typedef Unit<INPUT, OUTPUT> BASE;
-    Sub() : BASE() { BASE::name("Sub"); }
-    Sub(size_t n_inputs, size_t n_outputs) : BASE(n_inputs, n_outputs) { BASE::name("Sub"); }
-    Sub(vector<shared_ptr<INPUT>> &inputs, vector<shared_ptr<OUTPUT>> &outputs) : BASE(inputs, outputs) { BASE::name("Sub"); };
-    Sub(vector<shared_ptr<OUTPUT>> &outputs, size_t n_outputs) : BASE(outputs, n_outputs) { BASE::name("Sub"); };
-
-    OpResult run() override {
-      if (BASE::inputs().size() < 2)
-        return OpResult(1, "No enough parameters");
-
-      if (BASE::outputs().size() < 1)
-        return OpResult(2, "No enough space to store outputs");
-
-      *BASE::outputs()[0] = *BASE::inputs()[0] - (*BASE::inputs()[1]);
-      return OpResult(0, "Output updated");
-    }
-};
-
-/* Perform multiplication on inputs */
-template<typename INPUT, typename OUTPUT>
-class Mul : public Unit<INPUT, OUTPUT> {
-  public:
-    typedef Mul<INPUT, OUTPUT> TYPE;
-    typedef Unit<INPUT, OUTPUT> BASE;
-    Mul() : BASE() { BASE::name("Mul"); }
-    Mul(size_t n_inputs, size_t n_outputs) : BASE(n_inputs, n_outputs) { BASE::name("Mul"); }
-    Mul(vector<shared_ptr<INPUT>> &inputs, vector<shared_ptr<OUTPUT>> &outputs) : BASE(inputs, outputs) { BASE::name("Mul"); };
-    Mul(vector<shared_ptr<OUTPUT>> &outputs, size_t n_outputs) : BASE(outputs, n_outputs) { BASE::name("Mul"); };
-
-    OpResult run() override {
-      if (BASE::inputs().size() < 2)
-        return OpResult(1, "No enough parameters");
-
-      if (BASE::outputs().size() < 1)
-        return OpResult(2, "No enough space to store outputs");
-
-      *BASE::outputs()[0] = *BASE::inputs()[0] * (*BASE::inputs()[1]);
-      return OpResult(0, "Output updated");
-    }
-};
-
-/* Perform division on inputs */
-template<typename INPUT, typename OUTPUT>
-class Div : public Unit<INPUT, OUTPUT> {
-  public:
-
-    typedef Div<INPUT, OUTPUT> TYPE;
-    typedef Unit<INPUT, OUTPUT> BASE;
-    Div() : BASE() { BASE::name("Div"); }
-    Div(size_t n_inputs, size_t n_outputs) : BASE(n_inputs, n_outputs) { BASE::name("Div"); }
-    Div(vector<shared_ptr<INPUT>> &inputs, vector<shared_ptr<OUTPUT>> &outputs) : BASE(inputs, outputs) { BASE::name("Div"); };
-    Div(vector<shared_ptr<OUTPUT>> &outputs, size_t n_outputs) : BASE(outputs, n_outputs) { BASE::name("Div"); };
-
-    OpResult run() override {
-      if (BASE::inputs().size() < 2)
-        return OpResult(1, "No enough parameters");
-
-      if (BASE::outputs().size() < 1)
-        return OpResult(2, "No enough space to store outputs");
-
-      if (*BASE::inputs()[1] == 0)
-        return OpResult(3, "Divided by zero");
-
-      *BASE::outputs()[0] = *BASE::inputs()[0] / (*BASE::inputs()[1]);
-      return OpResult(0, "Output updated");
-    }
-};
-
-/* Perform modulus on inputs */
-template<typename INPUT, typename OUTPUT>
-class Mod : public Unit<INPUT, OUTPUT> {
-  public:
-    typedef Mod<INPUT, OUTPUT> TYPE;
-    typedef Unit<INPUT, OUTPUT> BASE;
-    Mod() : BASE() { BASE::name("Mod"); }
-    Mod(size_t n_inputs, size_t n_outputs) : BASE(n_inputs, n_outputs) { BASE::name("Mod"); }
-    Mod(vector<shared_ptr<INPUT>> &inputs, vector<shared_ptr<OUTPUT>> &outputs) : BASE(inputs, outputs) { BASE::name("Mod"); };
-    Mod(vector<shared_ptr<OUTPUT>> &outputs, size_t n_outputs) : BASE(outputs, n_outputs) { BASE::name("Mod"); };
-
-    OpResult run() override {
-      if (BASE::inputs().size() < 2)
-        return OpResult(1, "No enough parameters");
-
-      if (BASE::outputs().size() < 1)
-        return OpResult(2, "No enough space to store outputs");
-
-      if (*BASE::inputs()[1] == 0)
-        return OpResult(3, "Divided by zero");
-
-      *BASE::outputs()[0] = *BASE::inputs()[0] % *BASE::inputs()[1];
-      return OpResult(0, "Output updated");
-    }
-};
-
-/* Perform negation on input */
-template<typename INPUT, typename OUTPUT>
-class Negate : public Unit<INPUT, OUTPUT> {
-  public:
-    typedef Negate<INPUT, OUTPUT> TYPE;
-    typedef Unit<INPUT, OUTPUT> BASE;
-    Negate() : BASE() { BASE::name("Negate"); }
-    Negate(size_t n_inputs, size_t n_outputs) : BASE(n_inputs, n_outputs) { BASE::name("Negate"); }
-    Negate(vector<shared_ptr<INPUT>> &inputs, vector<shared_ptr<OUTPUT>> &outputs) : BASE(inputs, outputs) { BASE::name("Negate"); };
-    Negate(vector<shared_ptr<OUTPUT>> &outputs, size_t n_outputs) : BASE(outputs, n_outputs) { BASE::name("Negate"); };
-
-    OpResult run() override {
-      if (BASE::inputs().size() < 1)
-        return OpResult(1, "No enough parameters");
-
-      if (BASE::outputs().size() < 1)
-        return OpResult(2, "No enough space to store outputs");
-
-      *BASE::outputs()[0] = -(*BASE::inputs()[0]);
-      return OpResult(0, "Output updated");
-    }
-};
 
 } // namespace Incredible 
