1b54453c413c092043588ac9e6e1f645b6ea6df5
Zex Li <dbloodwxp@gmail.com>
2018-08-07 20:07:01 +0800

    load key

--- truth/src/truth/app/test.go
+++ truth/src/truth/app/test.go
@@ -42,8 +42,7 @@ func do_rsa() {
   //rd := strings.NewReader("NASA IS GOING TO MARS")
   prikey, err := rsa.GenerateKey(rd, 1024)
   if err != nil {
-    glog.Error("failed to generate prikey", err)
-    return
+    glog.Fatal("failed to generate prikey", err)
   }
 
   pubkey := prikey.Public()
@@ -56,16 +55,14 @@ func do_rsa() {
   msg := []byte("NASA IS GOING TO MARS")
   en_msg, err := rsa.EncryptOAEP(sha256.New(), rd, &prikey.PublicKey, msg, nil)
   if err != nil {
-    glog.Error("failed to encrypt msg: ", err)
-    return
+    glog.Fatal("failed to encrypt msg: ", err)
   }
 
   glog.Info(en_msg, len(en_msg))
 
   de_msg, err := rsa.DecryptOAEP(sha256.New(), rd, prikey, en_msg, nil)
   if err != nil {
-    glog.Error("failed to decrypt msg: ", err)
-    return
+    glog.Fatal("failed to decrypt msg: ", err)
   }
 
   glog.Info("expect: ", msg)
@@ -73,23 +70,48 @@ func do_rsa() {
   glog.Info(string(de_msg), len(de_msg))
 
   dump_key(prikey)
+  //load_key()
 }
 
 func dump_key(prikey *rsa.PrivateKey) {
   pem_pri := pem.EncodeToMemory(&pem.Block{
-    Type: "Truth RSA Private",
+    Type: "Truth RSA Private Key",
     Bytes: x509.MarshalPKCS1PrivateKey(prikey),
   })
   ioutil.WriteFile("pri.key", pem_pri, 0644)
 
   pem_pub := pem.EncodeToMemory(&pem.Block{
-    Type: "Truth RSA Public",
+    Type: "Truth RSA Public Key",
     Bytes: x509.MarshalPKCS1PublicKey(&prikey.PublicKey),
   })
   ioutil.WriteFile("pub.key", pem_pub, 0644)
 }
 
 func load_key() {
+  pem_pub, err := ioutil.ReadFile("pub.key")
+  if err != nil {
+    glog.Fatal("failed to read public key: ", err)
+  }
+  /**
+  pem_pri, err := ioutil.ReadFile("pri.key")
+  if err != nil {
+    glog.Error("failed to read private key: ", err)
+    return nil, nil
+  }
+  */
+  blk, rest := pem.Decode(pem_pub)
+  glog.Info(blk.Type)
+
+  if blk == nil {
+    glog.Fatal("invalid public key")
+  }
+
+  pubkey, err := x509.ParsePKIXPublicKey(blk.Bytes)
+  if err != nil {
+    glog.Fatal("failed to parse pem public key", err)
+  }
+  glog.Info(pubkey)
+  glog.Info(rest)
 }
 
 func main() {
