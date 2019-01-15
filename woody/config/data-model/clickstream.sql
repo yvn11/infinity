4951e66bbced38b2f28da4bca20d512184adc229
Zex Li <dbloodwxp@gmail.com>
2019-01-15 20:21:19 +0800

    [WIP] time delta

--- woody/config/data-model/clickstream.sql
+++ woody/config/data-model/clickstream.sql
@@ -1,10 +1,12 @@
-CREATE KEYSPACE IF NOT EXISTS woody WITH replication = {'class':'SimpleStrategy','replication_factor':3};
+CREATE KEYSPACE IF NOT EXISTS woody_clickstream WITH replication = {'class':'SimpleStrategy','replication_factor':3};
 
-USE woody;
+USE woody_clickstream;
+
+/** events */
 
 CREATE TABLE IF NOT EXISTS click_event (
   session_id varchar,
-  ts varchar,
+  ts timestamp,
   item_id varchar,
   category varchar,
   PRIMARY KEY (session_id, ts)
@@ -12,8 +14,45 @@ CREATE TABLE IF NOT EXISTS click_event (
 
 CREATE TABLE IF NOT EXISTS buy_event (
   session_id varchar,
-  ts varchar,
+  ts timestamp,
   item_id varchar,
-  price varchar,
+  price float,
+  quantity int,
   PRIMARY KEY (session_id, ts)
+);
+
+/** aggregations */
+
+CREATE TABLE IF NOT EXISTS session_quan (
+  session_id varchar,
+  quan_bought int,
+  PRIMARY KEY (session_id)
+);
+
+CREATE TABLE IF NOT EXISTS item_quan (
+  item_id varchar,
+  quan_bought int,
+  PRIMARY KEY (item_id)
+);
+
+CREATE TABLE IF NOT EXISTS item_click (
+  item_id varchar,
+  click_count int,
+  PRIMARY KEY (item_id)
+);
+
+CREATE TABLE IF NOT EXISTS session_click (
+  session_id varchar,
+  click_count int,
+  PRIMARY KEY (session_id)
+);
+
+CREATE TABLE IF NOT EXISTS purchase_delta (
+  session_id varchar,
+  item_id varchar,
+  ts_buy timestamp,
+  ts_click timestamp,
+  ts_delta timestamp,
+  price float,
+  PRIMARY KEY (session_id, item_id)
 );
--- woody/woody/apps/clickstream.py
+++ woody/woody/apps/clickstream.py
@@ -2,7 +2,7 @@ from pyspark.context import SparkContext
 from pyspark.conf import SparkConf
 from pyspark.streaming.context import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-from pyspark.sql import Row, SparkSession
+from pyspark.sql import Row, SparkSession, functions
 
 from woody.common.config import Config
 from py4j.protocol import Py4JJavaError
@@ -34,6 +34,7 @@ class ClickStreamAggr(object):
         self._sc.setLogLevel('WARN')
         self._ssc = StreamingContext(self._sc, Config.ssc_duration)
         self._sess = SparkSessionInstance(conf)
+        self._keyspace = "woody_clickstream"
         self._from_kfk()
 
 
@@ -45,45 +46,190 @@ class ClickStreamAggr(object):
         self._click_ds = KafkaUtils.createDirectStream(self._ssc, ["click_ev"], kfk_params)
         self._buy_ds = KafkaUtils.createDirectStream(self._ssc, ["buy_ev"], kfk_params)
         """
-        fmt = "%Y-%m-%d %H:%M:%S"
-        dates = self._ds.map(lambda x: datetime.strptime(
-            ''.join(x[0].split('.')[:1]), fmt))
-        dates = dates.map(lambda x: (x, 1)).reduceByKey(lambda x, y: y+x)
-        dates.pprint()
-        """
-        """
         citi = self._sess.read.format("org.apache.spark.sql.cassandra")\
-            .options(table="citizenship", keyspace="woody")\
+            .options(table="citizenship", keyspace=self._keyspace)\
             .load()
         citi.show()
+        #dates.saveAsTextFiles(self.__class__.__name__)
         """
+        # 2014-04-07T10:51:09.277Z
+        fmt = "%Y-%m-%dT%H:%M:%S.%fZ"
         click = self._click_ds.map(lambda x: json.loads(x[1]))
-        click.pprint(100)
-        #self.click_df = click.read.cassandraFormat("click_event", "woody")
-        #click.foreachRDD(self.persist_click)
+        def click_conv(x):
+            x['timestamp'] = datetime.strptime(x['timestamp'], fmt)
+            return x
+
+        click = click.map(click_conv)
+        #click.count().pprint()
+        click.foreachRDD(self.persist_click)
+
+        self.aggr_session_click(click)
+        self.aggr_item_click(click)
 
         buy = self._buy_ds.map(lambda x: json.loads(x[1]))
-        buy.pprint()
 
-        # delta of buy timestamp and click timestamp
-#        click_ts = buy.map(lambda x: (x['session_id'], int(x['timestamp'])))
-#        buy_ts = buy.map(lambda x: (x['session_id'], int(x['timestamp'])))
-        # session => buy quantity
-        sess_quan = buy.map(lambda x: (x['session_id'], int(x['quantity']))).reduceByKey(lambda x,y: y+x)
-        sess_quan.pprint()
-        # item => buy quantity
-        item_quan = buy.map(lambda x: (x['item_id'], int(x['quantity']))).reduceByKey(lambda x,y: y+x)
-        item_quan.pprint()
+        def buy_conv(x):
+            x['price'] = float(x['price'])
+            x['quantity'] = int(x['quantity'])
+            x['timestamp'] = datetime.strptime(x['timestamp'], fmt)
+            return x
+        buy = buy.map(buy_conv)
+        buy.foreachRDD(self.persist_buy)
+        
+        self.aggr_buy_click_delta(click, buy)
+        self.aggr_session_quan(buy)
+        self.aggr_item_quan(buy)
 
-        #dates.saveAsTextFiles(self.__class__.__name__)
-    def foreach_rdd(self, time, rdd):
-        print(time, rdd)
+    def aggr_buy_click_delta(self, click, buy):
+        """delta of buy timestamp and click timestamp
+        TODO(zex): two test datasets doesn't come up in order of time
+        """
+        def earlist_click(x, y):
+            if not x[1]:
+                return y[1]
+            if not y[1]:
+                return x[1]
+            if x[1] < y[1]: # timestamp
+                return x
+            return y
+
+        def time_delta(x, y):
+            if x[1] and y[1]:
+                if y[1] > x[1]:
+                    return y[1]-x[1]
+                return x[1]-y[1]
+            return None
+        
+        def split_iid(iid):
+            return iid.split('-')
+
+        def persist(time, rdd):
+            if len(rdd.collect()) == 0:
+                return
+
+            try:
+                print(rdd.collect())
+                df = self._sess.createDataFrame(rdd, ["iid", "ts_click", "ts_buy"]).fillna(datetime.min)
+                cols = functions.split(df["iid"], '-')
+                df["session_id"], df["item_id"] = cols[0], cols[1]
+
+                full_df = df.dropna()
+                full_df["ts_delta"] = full_df["ts_buy"] - full_df["ts_click"]
+                full_df.show()
+                full_df.write.format("org.apache.spark.sql.cassandra")\
+                    .options(table="purchase_delta", keyspace=self._keyspace)\
+                    .save(mode='append')
+
+                df = df.select([c for c in df.columns() if c in ("item_id", "price", "ts_click", "ts_buy", "session_id", "ts_delta")])
+                df.show()
+
+                #self._sess.createDataFrame(rdd, ["item_id", "price", "session_id", "ts_delta"])
+                df.write.format("org.apache.spark.sql.cassandra")\
+                    .options(table="purchase_delta", keyspace=self._keyspace)\
+                    .save(mode='append')
+            except Exception as e:
+                print("aggr_buy_click_delta persist failed", e)
+
+        click_ts = click.map(lambda x: ('-'.join([x['session_id'], x['item_id']]), x['timestamp']))\
+                .reduce(earlist_click)
+        buy_ts = buy.map(lambda x: ('-'.join([x['session_id'], x['item_id']]), x['timestamp']))\
+                .reduce(earlist_click)
+
+        delta_ts = click_ts.fullOuterJoin(buy_ts)
+        delta_ts.pprint(100)
+        delta_ts.foreachRDD(persist)
+
+    def aggr_session_quan(self, buy):
+        """session => quantity bought"""
+        def persist(time, rdd):
+            if len(rdd.collect()) == 0:
+                return
+
+            try:
+                self._sess.createDataFrame(rdd, ["session_id", "quan_bought"])\
+                    .write.format("org.apache.spark.sql.cassandra")\
+                    .options(table="session_quan", keyspace=self._keyspace)\
+                    .save(mode='append')
+            except Exception as e:
+                print("session_quan persist failed", e)
+
+        sess_quan = buy.map(lambda x: (x['session_id'], x['quantity']))\
+                .reduceByKey(lambda x,y: y+x)
+        sess_quan.foreachRDD(persist)
+
+    def aggr_session_click(self, click):
+        """session => click count"""
+        def persist(time, rdd):
+            if len(rdd.collect()) == 0:
+                return
+
+            try:
+                self._sess.createDataFrame(rdd, ["session_id", "click_count"])\
+                    .write.format("org.apache.spark.sql.cassandra")\
+                    .options(table="session_click", keyspace=self._keyspace)\
+                    .save(mode='append')
+            except Exception as e:
+                print("session_click persist failed", e)
+
+        click.map(lambda x: (x['session_id'], 1)).reduceByKey(lambda x,y: y+x)\
+            .foreachRDD(persist)
+
+    def aggr_item_click(self, click):
+        """item => click count"""
+        def persist(time, rdd):
+            if len(rdd.collect()) == 0:
+                return
+
+            try:
+                self._sess.createDataFrame(rdd, ["item_id", "click_count"])\
+                    .write.format("org.apache.spark.sql.cassandra")\
+                    .options(table="item_click", keyspace=self._keyspace)\
+                    .save(mode='append')
+            except Exception as e:
+                print("item_click persist failed", e)
+
+        click.map(lambda x: (x['item_id'], 1)).reduceByKey(lambda x,y: y+x)\
+            .foreachRDD(persist)
+
+    def aggr_item_quan(self, buy):
+        """item => quantity bought"""
+        def persist(time, rdd):
+            if len(rdd.collect()) == 0:
+                return
+            try:
+                self._sess.createDataFrame(rdd, ["item_id", "quan_bought"])\
+                    .write.format("org.apache.spark.sql.cassandra")\
+                    .options(table="item_quan", keyspace=self._keyspace)\
+                    .save(mode='append')
+            except Exception as e:
+                print("item_quan persist failed", e)
+
+        item_quan = buy.map(lambda x: (x['item_id'], x['quantity'])).reduceByKey(lambda x,y: y+x)
+        item_quan.foreachRDD(persist)
 
     def persist_click(self, time, rdd):
-        taken = rdd.take(len(rdd.collect()))
-        for t in taken:
-            print(t)
-            self.click_df.options(t).save()
+        if len(rdd.collect()) == 0:
+            return
+        print('click', len(rdd.collect())) 
+        try:
+            self._sess.createDataFrame(rdd, ["category", "item_id", "session_id", "ts"])\
+                .write.format("org.apache.spark.sql.cassandra")\
+                .options(table="click_event", keyspace=self._keyspace)\
+                .save(mode='append')
+        except Exception as e:
+            print("persist failed", e)
+
+    def persist_buy(self, time, rdd):
+        if len(rdd.collect()) == 0:
+            return
+        print('buy', len(rdd.collect())) 
+        try:
+            self._sess.createDataFrame(rdd, ["item_id", "price", "quantity", "session_id", "ts"])\
+                .write.format("org.apache.spark.sql.cassandra")\
+                .options(table="buy_event", keyspace=self._keyspace)\
+                .save(mode='append')
+        except Exception as e:
+            print("buy_event persist failed", e)
         
     def run(self):
         try:
