96bceb3fed4330dd271df61f9a39ff826ab53b72
Zex Li <dbloodwxp@gmail.com>
2019-01-28 19:36:51 +0800

    dns config

--- woody/scripts/kube/adminit.sh
+++ woody/scripts/kube/adminit.sh
@@ -13,17 +13,17 @@ kubeadm init \
     --pod-network-cidr=10.244.0.0/16 \
     --service-cidr=10.244.10.0/16 \
     -v 256 \
-    --ignore-preflight-errors SystemVerification,DirAvailable--var-lib-etcd,\
-FileAvailable--etc-kubernetes-manifests-kube-apiserver.yaml,\
-FileAvailable--etc-kubernetes-manifests-kube-controller-manager.yaml,\
-FileAvailable--etc-kubernetes-manifests-kube-scheduler.yaml,\
-FileAvailable--etc-kubernetes-manifests-etcd.yaml,\
-Port-6443,\
-Port-10250,\
-Port-2379,\
-Port-10252,\
-Port-2380,\
-Port-10251
+    --ignore-preflight-errors SystemVerification,DirAvailable--var-lib-etcd
+#FileAvailable--etc-kubernetes-manifests-kube-apiserver.yaml,\
+#FileAvailable--etc-kubernetes-manifests-kube-controller-manager.yaml,\
+#FileAvailable--etc-kubernetes-manifests-kube-scheduler.yaml,\
+#FileAvailable--etc-kubernetes-manifests-etcd.yaml,\
+#Port-6443,\
+#Port-10250,\
+#Port-2379,\
+#Port-10252,\
+#Port-2380,\
+#Port-10251
 
 if [[ -f /etc/kubernetes/admin.conf ]] ;then
   cp /etc/kubernetes/admin.conf ~/.kube/config
